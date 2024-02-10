cda2bcffdef2b8d0e69d7585473c3c15052c45c0
Hecate2 <2474101468@qq.com>
2024-02-10 17:55:36 +0800

    omit /api/v1 in test

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ def anyio_backend():
 @pytest.fixture(scope="module")
 async def client():
     async with LifespanManager(app):
-        async with AsyncClient(app=app, base_url="http://test") as c:
+        async with AsyncClient(app=app, base_url="http://test/api/v1/") as c:
             yield c
 
 
@@ -26,32 +26,32 @@ async def client():
 async def test_create_user(client: AsyncClient):  # nosec
     await drop_tables()
     await init_tables()
-    response = await client.get("/api/v1/orgs")
+    response = await client.get("orgs")
     assert response.status_code == 200 and response.json() == []
-    response = await client.get("/api/v1/orgs/a")
+    response = await client.get("orgs/a")
     assert response.status_code == 200 and response.text == 'null' and response.json() == None
     
     org_names = {'test_org', 'Hospital1', 'hospital2'}
     for org in org_names:
-        response = await client.post(f"/api/v1/orgs/{org}")
+        response = await client.post(f"orgs/{org}")
         assert response.status_code == 200, response.json() == {'success': True}
     
-    result = (await client.get("/api/v1/orgs")).json()
+    result = (await client.get("orgs")).json()
     assert len(result) == len(org_names)
     for org in result:
         assert org['name'] in org_names
     filter = 'Hospital'
-    result = (await client.get(f"/api/v1/orgs?filter={filter}")).json()
+    result = (await client.get(f"orgs?filter={filter}")).json()
     for org in result:
         assert filter.lower() in org['name'].lower()
-    result = (await client.get(f"/api/v1/orgs?filter={filter}&case=1")).json()
+    result = (await client.get(f"orgs?filter={filter}&case=1")).json()
     for org in result:
         assert filter in org['name']
 
-    result = (await client.get("/api/v1/orgs/a")).json()
+    result = (await client.get("orgs/a")).json()
     assert result is None
     random_org = random.choice(list(org_names))
-    result = (await client.get(f"/api/v1/orgs/{random_org}")).json()
+    result = (await client.get(f"orgs/{random_org}")).json()
     assert (result['name'] == random_org
         and abs(parser.parse(result['datetime']) - datetime.datetime.utcnow()).total_seconds()
             - datetime.timedelta(hours=8).total_seconds()) < 10
